bb3c6ccc4d2d401ffb4fb88ed560aefbae48f28b
QuachDuyHung1998 <quachduyhung.1998@gmail.com>
2020-12-24 21:24:17 +0700

    add data on local and call api get weather

---
+++ lib/utils/NetworkUtil.dart
@@ -0,0 +1,7 @@
+const BASE_URL = "https://openweathermap.org/data/2.5";
+
+const APP_ID = "97b44e3f12f4e66a8c7f07a97606184a";
+const WEATHER = "weather";
+
+const QUERY_PARAM = "q";
+const APP_ID_PARAM = "appid";
